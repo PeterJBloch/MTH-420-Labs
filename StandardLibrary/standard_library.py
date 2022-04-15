d043397e78f66e287ddb3ea6cbaae5f8a621f7e1
PeterJBloch <peterjbloch@gmail.com>
2022-04-15 11:05:23 -0700

    updated standard library to contain shut the box code

--- StandardLibrary/standard_library.py
+++ StandardLibrary/standard_library.py
@@ -4,8 +4,9 @@ Peter J. Bloch
 MTH 420
 15 April 2022
 """
-import calculator
+import calculator, time
 from itertools import chain, combinations
+from random import randint
 
 # Problem 1
 def prob1(L):
@@ -84,10 +85,77 @@ def power_set(A):
         (list(sets)): The power set of A as a list of sets.
     """
     # raise NotImplementedError("Problem 4 Incomplete")
-        eturn chain.from_iterable(combinations(some_list, index) for index in range(len(some_list)+1))    
+    return chain.from_iterable(combinations(A, index) for index in range(len(A)+1))    
 
 
 
 # Problem 5: Implement shut the box.
 def shut_the_box(player, timelimit):
     """Play a single game of shut the box."""
+    #Start the game
+    print("\nWelcome {}, you have {} seconds to play the game!".format(player, timelimit))
+    numbers = [1,2,3,4,5,6,7,8,9]
+    time_remaining = timelimit
+    while True:
+        numbers, time_taken = timelimit(numbers, time_remaining, player)
+        time_remaining-=time_taken
+        if time_remaining<0:
+            print("\nSorry {}, the game time elapsed. Better luck next time!".format(player))
+            return
+        if numbers == []:
+            print("\nCongratulations {}! You won with {} seconds remaining!".format(player, time_remaining))
+    return
+
+def roll_dice(numbers):
+    roll = 0
+    #Roll first dice
+    roll += randint(1,6)
+
+    #If the sum of remaining numbers is less than six, proceed
+    if sum(numbers)<6:
+        return
+
+    #Roll second dice
+    else:
+        roll+=randint(1,6)
+    return roll
+
+def play_round(numbers, seconds_remaining, name):
+    print("The remaining numbers available are: {}".format(numbers))
+    roll = roll_dice(numbers)
+
+    #Check if there is a viable combination of leftover numbers for the roll
+    found_combo = False
+    #Use the power set from previous problem
+    powerset = list(power_set(numbers))
+    # print("Powerset left: {}".format(powerset))
+    for combo in powerset:
+        if sum(combo) == roll:
+            found_combo = True
+    if found_combo == False:
+        print("\nSorry {}, you rolled a {} and there are no more combinations left. Better luck next time!".format(name, roll))
+        exit(0)
+    print("Your roll was {}, and you have {} seconds left to play the game".format(roll, seconds_remaining))
+    start = time.time() #Get current time
+    #User can make decisions now
+    making_selection = True
+    selection = []
+    while making_selection:
+        selection = input("What number(s) would you like to remove next: ").split(" ")
+        for i in range(len(selection)):
+            try:
+                selection[i] = int(selection[i])
+            except:
+                print("{} could not be made into a number.".format(i))
+                continue
+            if selection[i] not in numbers:
+                print("This number is not available: {}".format(int(i)))
+                continue
+        # print(new_selection)
+        if sum(selection) == roll:
+            making_selection = False
+        else:
+            print("Those numbers add to {}, not your roll: {}".format(sum(selection), roll))
+    for i in selection:
+        numbers.remove(i)
+    return numbers, time.time()-start #The remaining unchosen numbers and the time difference of user decision is returned
