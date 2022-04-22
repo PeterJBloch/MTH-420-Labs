89ae7137d5d1189bc6f1b110187012e75c58aab1
PeterJBloch <peterjbloch@gmail.com>
2022-04-22 10:08:24 -0700

    fixed problem 4

--- PythonAndNumpy1/python_numpy1.py
+++ PythonAndNumpy1/python_numpy1.py
@@ -37,8 +37,8 @@ def prob4():
     A = np.array([[3,-1,4],[1,5,-9]])
     B = np.array([[2,6,-5,3],[5,-8,9,7],[9,-3,-2,-3]])
     product = np.dot(A,B)
-    print(product)
-    return
+    # print(product)
+    return product
 
 def prob5():
     print("\nProblem 5:")
